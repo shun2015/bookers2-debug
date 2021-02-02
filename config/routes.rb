5a59273b58037d3c7a37c0a78f96c30baccfc88d
shun2015 <dwat.pmjg@gmail.com>
2021-02-02 06:19:26 +0000

    Update

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'home/about' => 'homes#about'
   resources :users,only: [:show,:index,:edit,:update]
   resources :books do
+    resource :favorites, only: [:create, :destroy]
     resources :post_comments, only: [:create, :destroy]
   end
 end
