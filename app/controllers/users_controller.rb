1637a6f4c5d95f1996773e0f98877e4ae9b1f8a9
shun2015 <dwat.pmjg@gmail.com>
2021-02-07 07:27:53 +0000

    [Add] 住所検索機能

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,6 +33,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @users = @user.followers.all
   end
+  
+  def zipedit
+  params.require(:user).permit(:postcode, :prefecture_name, :address_city, :address_street, :address_building)
+  end
 
   private
   def user_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_03_140612) do
+ActiveRecord::Schema.define(version: 2021_02_07_032919) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -42,19 +42,7 @@ ActiveRecord::Schema.define(version: 2021_02_03_140612) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.string "name"
-    t.text "introduction"
-    t.string "profile_image_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
+# Could not dump table "users" because of following StandardError
+#   Unknown type 'strin' for column 'address_building'
 
 end
